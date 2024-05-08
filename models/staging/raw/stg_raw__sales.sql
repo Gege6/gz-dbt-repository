352b033835c13cd0bb4d5b2ceb08e0de696d7c10
Géraldine Casanova <geraldine.casanova@gmail.com>
2024-05-08 08:18:02 +0000

    premier commit

---
+++ models/staging/raw/stg_raw__sales.sql
@@ -0,0 +1,23 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'sales') }}
+
+),
+
+renamed as (
+
+    select
+        date_date,
+        orders_id,
+        pdt_id AS products_id,
+        CONCAT(orders_id,"_",pdt_id) AS orders_product_id,
+        revenue,
+        CAST(quantity AS FLOAT64) AS quantity
+
+    from source
+
+)
+
+select * from renamed
