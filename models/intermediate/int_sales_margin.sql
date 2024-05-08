f488c563bd0ff48919872471816b6b3c1b321949
Géraldine Casanova <geraldine.casanova@gmail.com>
2024-05-08 10:07:45 +0000

    round margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,7 +4,7 @@ date_date,
 revenue,
 quantity,
 quantity * purchase_price AS purchase_cost,
-revenue - (quantity * purchase_price) AS margin
+ROUND(revenue - (quantity * purchase_price),2) AS margin
 FROM {{ ref('stg_raw__sales') }} AS sales
 LEFT JOIN {{ ref('stg_raw__product') }} AS product
 ON sales.products_id = product.products_id
