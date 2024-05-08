03128be396f58d1a658bbd1c4c9dc5e889d17a4c
Géraldine Casanova <geraldine.casanova@gmail.com>
2024-05-08 09:59:03 +0000

    test sur le test de fraicheur

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -12,7 +12,7 @@ renamed as (
         date_date,
         orders_id,
         pdt_id AS products_id,
-        CONCAT(orders_id,"_",pdt_id) AS orders_product_id,
+        CONCAT(date_date,"_",orders_id,"_",pdt_id) AS date_date_orders_product_id,
         revenue,
         CAST(quantity AS FLOAT64) AS quantity
 
